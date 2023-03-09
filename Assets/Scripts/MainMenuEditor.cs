fef8d2c63384a1053623c52f7ecea30d6c203724
Ksangillo <kasangillo@email.neit.edu>
2023-03-09 10:04:22 -0500

    Instructions
    Fixed Instruction Screen

--- Assets/Scripts/MainMenuEditor.cs
+++ Assets/Scripts/MainMenuEditor.cs
@@ -7,6 +7,11 @@ public class MainMenuEditor : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject Instructions;
+    public GameObject controller;
+    public GameObject keyboard;
+    public GameObject switchToControllerMethod;
+    public GameObject switchToKeyboardMethod;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +30,52 @@ public class MainMenuEditor : MonoBehaviour
         {
             mainMenu.SetActive(true);
             Instructions.SetActive(false);
+            controller.SetActive(false);
+            //switchToControllerMethod.SetActive(true);
+           // switchToKeyboardMethod.SetActive(false);
         }
         else
         {
             mainMenu.SetActive(false);
             Instructions.SetActive(true);
+            keyboard.SetActive(true);
+            //switchToControllerMethod.SetActive(false);
+           // switchToKeyboardMethod.SetActive(true);
+        }
+    }
+
+    public void SwitchControllerScheme()
+    {
+        if (Instructions.activeInHierarchy == true)
+        {
+            controller.SetActive(true);
+            keyboard.SetActive(false);
+           // switchToControllerMethod.SetActive(true);
+            //switchToKeyboardMethod.SetActive(false);
+        }
+        else
+        {
+            controller.SetActive(false);
+            keyboard.SetActive(true);
+           // switchToControllerMethod.SetActive(false);
+           // switchToKeyboardMethod.SetActive(true);
+        }
+    }
+    public void SwitchKeyboardScheme()
+    {
+        if (Instructions.activeInHierarchy == true)
+        {
+            keyboard.SetActive(true);
+            controller.SetActive(false);
+           // switchToControllerMethod.SetActive(false);
+           // switchToKeyboardMethod.SetActive(true);
+        }
+        else
+        {
+            keyboard.SetActive(false);
+            controller.SetActive(true);
+           // switchToControllerMethod.SetActive(true);
+            //switchToKeyboardMethod.SetActive(false);
         }
     }
 }
